66628eefba2e8eab742f2d508fbd9580ceef4bd8
nabilchaib <nabilchaibdraa@gmail.com>
2023-01-30 12:22:29 -0500

    Version with Promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,52 +12,64 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    res.json(books);
+    return new Promise((resolve, reject) => {
+        axios.get('http://localhost:3000/books')
+        .then(response => {
+            res.json(response.data);
+            resolve();
+        })
+        .catch(error => {
+            res.status(404).json({message: "Books not found"});
+            reject(error);
+        });
     });
+});
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     let isbn = req.params.isbn;
-    let book = books[isbn];
-    if(!book) {
-    return res.status(404).json({message: "Book not found"});
-    }
-    res.json(book);
+    return new Promise((resolve, reject) => {
+        axios.get(`http://localhost:3000/books/${isbn}`)
+        .then(response => {
+            res.json(response.data);
+            resolve();
+        })
+        .catch(error => {
+            res.status(404).json({message: "Book not found"});
+            reject(error);
+        });
     });
-  
+});
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     let author = req.params.author;
-    let found = false;
-    let filtered_books = {}
-    Object.keys(books).forEach(function(key) {
-    if (books[key].author === author) {
-    filtered_books[key] = books[key];
-    found = true;
-    }
-    });
-    if(found){
-    return res.status(200).json(filtered_books);
-    }
-    else{
-    return res.status(404).json({message: "Book not found"});
-    }
-    });
-    
-    // Get all books based on title
-    public_users.get('/title/:title',function (req, res) {
-        let title = req.params.title;
-        let matchingBooks = [];
-        for (let key in books) {
-        if (books[key].title.toLowerCase() === title.toLowerCase()) {
-        matchingBooks.push(books[key]);
-        }
-        }
-        if (matchingBooks.length === 0) {
-        return res.status(404).json({ message: "No books found with the specified title" });
-        }
-        return res.status(200).json(matchingBooks);
+    return new Promise((resolve, reject) => {
+        axios.get(`http://localhost:3000/books?author=${author}`)
+        .then(response => {
+            res.json(response.data);
+            resolve();
+        })
+        .catch(error => {
+            res.status(404).json({message: "Book not found"});
+            reject(error);
         });
+    });
+});    
+    // Get all books based on title
+public_users.get('/title/:title',function (req, res) {
+    let title = req.params.title;
+    return new Promise((resolve, reject) => {
+        axios.get(`http://localhost:3000/books?title=${title}`)
+        .then(response => {
+            res.json(response.data);
+            resolve();
+        })
+        .catch(error => {
+            res.status(404).json({ message: "No books found with the specified title" });
+            reject(error);
+        });
+    });
+});
     
     // Get book review
     public_users.get('/review/:isbn',function (req, res) {
